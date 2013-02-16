0ea77a1623b048f38831242d29a65675c48715c6
Gareth burrows <gareth@breathehr.com>
2013-02-16 10:53:33 -0600

    fixed navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :orders, through: :item_order
 
-  validates_presence_of :email, :first_name, :is_vendor, :last_name, :password
+  validates_presence_of :email, :first_name, :last_name, :password
 
   validates_uniqueness_of :email
 end
