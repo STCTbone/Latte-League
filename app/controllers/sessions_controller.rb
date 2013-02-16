274204d7daf8a097adca9876c6d596c72b14b540
Will Piers <wpiers0405@gmail.com>
2013-02-16 12:28:09 -0600

    added some authentication functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,20 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
   def new
   end
 
   def create
-  	# user = User.authenticate(params[:email], params[:password])
+  	if user = User.authenticate(params[:email], params[:password])
+  		session[:user_id] = user.id
+  		session[:is_vendor] = user.is_vendor
+  		redirect_to items_url, notice: "Signed in!"
+  	else
+  		flash.now.alert = "Invalid email or password"
+  		render 'new'
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to items_url, :notice => "Logged out!"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :first_name, :last_name, :password
 
   validates_uniqueness_of :email
+
+  def User.authenticate(email, password)
+  	user = User.find_by_email(email)
+  	if user && user.password == password
+  		return user
+  	else
+  		nil
+  	end
+  end
 end
