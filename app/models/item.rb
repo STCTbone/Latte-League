3a3310681a8859605e4535efa34622c879cd7d1a
Matt Rieger <matthew.rieger@gmail.com>
2013-02-16 13:33:53 -0600

    Implemented currency into the price part of the Item model
    This was done entirely by Matt, Ankur was not involved.

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,17 @@ class Item < ActiveRecord::Base
 
   validates_presence_of :category, :is_available, :name, :price
   validates_uniqueness_of :name
+
+  def price=(price)
+    write_attribute(:price, price.to_f * 100)
+  end
+
+  def price
+    price = price_in_cents/100.to_f
+    ("%0.2f" % [price])
+  end
+
+  def price_in_cents
+    read_attribute(:price)
+  end
 end
