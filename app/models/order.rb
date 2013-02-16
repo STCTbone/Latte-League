ed701796562d38d86599a7e372be4422dd713f54
Will Piers <wpiers0405@gmail.com>
2013-02-16 16:09:14 -0600

    Added dependent: :destroy where needed in models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
   attr_accessible :instructions, :pickup_time, :status, :total_price, :user_id
 
-  has_many :item_orders
-  has_many :items, through: :item_orders
+  has_many :item_orders, dependent: :destroy
+  has_many :items, through: :item_orders, dependent: :destroy
 
   belongs_to :user
 
