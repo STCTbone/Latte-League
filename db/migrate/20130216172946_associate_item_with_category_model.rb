53fb97d94f3716674aee2a27672eafef82814882
Matt Rieger <matthew.rieger@gmail.com>
2013-02-16 11:38:59 -0600

    I am a jerk

--- db/migrate/20130216172946_associate_item_with_category_model.rb
+++ db/migrate/20130216172946_associate_item_with_category_model.rb
@@ -1,6 +1,6 @@
 class AssociateItemWithCategoryModel < ActiveRecord::Migration
   def change
-    change_table :item do |i|
+    change_table :items do |i|
         i.remove :category
         i.integer :category_id
     end
